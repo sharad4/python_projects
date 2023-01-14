2e6c3ec573c15bb2d7539c929ca17db83dce7c8a
sharad4 <sharad011989@gmail.com>
2023-01-14 23:02:20 +0530

    add snake

--- snake-game.py
+++ snake-game.py
@@ -1,7 +1,32 @@
 import PySimpleGUI as sg
+from time import time
+from random import randint
+
+def convert_pos_to_pixel(cell):
+  t1 = cell[0]*CELL_SIZE, cell[1]*CELL_SIZE
+  br = t1[0] + CELL_SIZE, t1[1] + CELL_SIZE
+  return t1, br
+
+def place_apple():
+  apple_pos = randint(0, CELL_NUM - 1), randint(0, CELL_NUM - 1)
+  while apple_pos in snake_body:
+    apple_pos = randint(0, CELL_NUM - 1), randint(0, CELL_NUM - 1)
+  return apple_pos
+
 
 # game constants
 FIELD_SIZE = 400
+CELL_NUM = 10
+CELL_SIZE = FIELD_SIZE / CELL_NUM
+
+# snake 
+snake_body = [(4,4), (3, 4), (2, 4)]
+DIRECTIONS = {'left':(-1,0), 'right':(1,0), 'up':(0,1), 'down':(0, -1)}
+direction = DIRECTIONS['up']
+
+# apple
+apple_pos = place_apple()
+apple_eaten = False
 
 sg.theme('Green')
 field = sg.Graph(
@@ -14,6 +39,7 @@ layout = [[field]]
 
 window = sg.Window('Snake', layout, return_keyboard_events=True)
 
+start_time = time()
 while True:
   event, values = window.read()
   if event == sg.WIN_CLOSED:
@@ -24,6 +50,38 @@ while True:
   if event == 'Right:39': print('right')
   if event == 'Down:40': print('down')
 
+  time_since_start = time() - start_time
+  if time_since_start >= 0.5:
+    start_time = time()
+
+    # apple snake collision
+    if snake_body[0] == apple_pos:
+      apple_pos = place_apple()
+      apple_eaten = True
+
+    # snake update
+    new_head = (snake_body[0][0] + direction[0], snake_body[0][1] + direction[1])
+    snake_body.insert(0, new_head)
+    if not apple_eaten:
+      snake_body.pop()
+    apple_eaten = False
+
+    # check death
+    if not 0 <= snake_body[0][0] <= CELL_NUM - 1 or \
+      not 0 <= snake_body[0][1] <= CELL_NUM - 1 or \
+        snake_body[0] in snake_body[1:]:
+        break
+
+    field.DrawRectangle((0,0), (FIELD_SIZE, FIELD_SIZE), 'black')
+
+    t1, br = convert_pos_to_pixel(apple_pos)
+    field.DrawRectangle(t1, br, 'red')
+
+    # draw snake
+    for index, part in enumerate(snake_body):
+      t1, br = convert_pos_to_pixel(part)
+      color = 'yellow' if index == 0 else 'green'
+      field.DrawRectangle(t1, br, color)
 
 
 window.close()
