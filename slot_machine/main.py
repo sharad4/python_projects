960041425750bf58a0bd82864930ecb22d6455c4
sharad4 <sharad011989@gmail.com>
2023-01-24 18:17:54 +0530

    add

--- slot_machine/main.py
+++ slot_machine/main.py
@@ -26,6 +26,19 @@ def get_slot_machine_spin(rows, cols, symbols):
     for row in range(rows):
       value = random.choice(all_symbols)
       current_symbols.remove(value)
+      column.append(value)
+    
+    columns.append(column)
+
+  return columns
+
+def print_slot_machine(columns):
+  for row in range(len(columns[0])):
+    for i, column in enumerate(columns):
+      if i != len(columns) - 1:
+        print(column[row], end=" | ")
+      else:
+        print(column[row], end="")
 
 def deposit():
   while True:
@@ -80,5 +93,8 @@ def main():
       break
 
   print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+  slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+  print_slot_machine(slots)
 
 main()
